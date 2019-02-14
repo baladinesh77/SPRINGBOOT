4ab914ebd9fcc80c837e53b0c793a2d6cf4ce445
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-02-14 12:58:23 +0530

    Updated List type

--- FunctionProject1/src/com/mahendra/DefaultMethodMain1.java
+++ FunctionProject1/src/com/mahendra/DefaultMethodMain1.java
@@ -2,13 +2,15 @@ package com.mahendra;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DefaultMethodMain1 {
 
 	public static void main(String[] args) {
-		List<Integer> listNumbers = Arrays.asList
-					(new Integer[]{10,22,23,90,234,134,722});
+		List<Integer> listNumbers = new LinkedList<Integer>();
+		listNumbers.addAll(Arrays.asList
+					(new Integer[]{10,22,23,90,234,134,722}));
 		for(int x: listNumbers) {
 			System.out.println(x);
 		}
