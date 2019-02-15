1b9c64898a67153041e21f97ae7e13b81ab416f1
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2019-02-15 15:24:07 +0530

    Maven spring project1

---
+++ demo1/src/main/java/com/mahendra/Application.java
@@ -0,0 +1,14 @@
+package com.mahendra;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Greeter greet=context.getBean(Greeter.class);
+		greet.greet("Mahendra");
+	}
+
+}
---
+++ demo1/src/main/java/com/mahendra/Greeter.java
@@ -0,0 +1,7 @@
+package com.mahendra;
+
+public class Greeter {
+	public void greet(String name) {
+		System.out.println("Welcome "+name);
+	}
+}
